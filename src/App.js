4793087f0586a72d4b665701b0fd9275449bd898
Emmanuel Tete Boye <emmanuelboye1957@gmail.com>
2023-10-01 09:14:57 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      {/* The rest of your app content goes here */}
+    </div>
+  );
+};
+
+export default App;
